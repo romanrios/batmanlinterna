da8b1bf32ba2e684c694dcaa0a4aeb81d91e68c2
Roman Rios <romanrios@live.com>
2024-04-04 02:52:00 -0300

    follow mouse

--- practica_mouse-follower/src/App.jsx
+++ practica_mouse-follower/src/App.jsx
@@ -1,42 +1,11 @@
-import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
+import FollowMouse from "./components/FollowMouse";
 
 function App() {
-  const [enabled, setEnabled] = useState(false);
-
-  useEffect(() => {
-    console.log("effect", { enabled });
-
-    const handleMove = (event) => {
-      const { clientX, clientY } = event;
-      console.log(clientX, clientY);
-    };
-    if (enabled) {
-      window.addEventListener("pointermove", handleMove);
-    }
-  }, [enabled]);
   return (
-    <>
-      <div
-        style={{
-          position: "absolute",
-          backgroundColor: "rgba(0, 0, 0, 0.5)",
-          border: "1px solid #fff",
-          borderRadius: "50%",
-          opacity: 0.8,
-          pointerEvents: "none",
-          left: -25,
-          top: -25,
-          width: 50,
-          height: 50,
-          // transform: `translate(${position.x}px, ${position.y}px)`,
-        }}
-      ></div>
-      <button onClick={() => setEnabled(!enabled)}>
-        {enabled ? "Desactivar" : "Activar"} seguir puntero
-      </button>
-    </>
+    <main>
+      <FollowMouse />
+    </main>
   );
 }
 
